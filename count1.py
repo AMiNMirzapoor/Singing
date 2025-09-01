6d835acf4fbf978ec2d8741237e7954e09b6619d
AMiN <mirzapoor.amin@gmail.com>
2025-09-01 20:18:49 +0200

    FEAT: Add Python code for the breathing count exercise

---
+++ count1.py
@@ -0,0 +1,30 @@
+import time
+
+
+n = int(input("Enter a number: "))
+
+# 1 second delay at the beginning
+time.sleep(1)
+
+# Growing cycles from 3 to n
+for k in range(3, n + 1):
+    for i in range(1, k + 1):
+        print(f"\033[92m{i}\033[0m", end=" ", flush=True)
+        time.sleep(1)
+    print()
+    for i in range(k, 0, -1):
+        print(f"\033[93m{i}\033[0m", end=" ", flush=True)
+        time.sleep(1)
+    print()
+
+# Shrinking cycles from n-1 down to 3
+for k in range(n - 1, 2, -1):
+    for i in range(1, k + 1):
+        print(f"\033[92m{i}\033[0m", end=" ", flush=True)
+        time.sleep(1)
+    print()
+    for i in range(k, 0, -1):
+        print(f"\033[93m{i}\033[0m", end=" ", flush=True)
+        time.sleep(1)
+    print()
+
